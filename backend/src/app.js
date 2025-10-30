e0a6a04ad841221a02b38852faf1c6a6d21cb24b
priyanshu-pal88 <aachipal88@gmail.com>
2025-10-30 20:36:40 +0530

    final

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://luxe-mart-phi.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS']
 }))
