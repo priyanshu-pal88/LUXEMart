0c1b023c36e1f16d8483a95ff9eb1fb8b0b9bf9d
priyanshu-pal88 <aachipal88@gmail.com>
2025-10-30 20:31:25 +0530

    final commit

--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import Cart from "../pages/cart";
+import Cart from "../pages/Cart";
 import Home from "../pages/Home";
 import UserProfile from "../pages/UserProfile";
+
+
 
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/cart' element={<Cart />} />
+      <Route path='/cart' element={<Cart/>} />
       <Route path='/user-profile' element={<UserProfile />} />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
